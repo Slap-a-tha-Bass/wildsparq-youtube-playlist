f37d56dabce587de561a460af232ba53d567772f
Corey DeLoach <cdeloach16@gmail.com>
2022-03-16 18:53:53 -0500

    add components/playlists, imported google font

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { PlaylistsComponent } from './components/playlists/playlists.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    PlaylistsComponent
   ],
   imports: [
     BrowserModule,
